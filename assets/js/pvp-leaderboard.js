d2773d2d5526672052aeae0fe19afc5c77557418
mit0224 <38598765+mti0224@users.noreply.github.com>
2026-09-19 10:29:03 +0800

    Place PvP star layers inside image fields

--- assets/js/pvp-leaderboard.js
+++ assets/js/pvp-leaderboard.js
@@ -345,8 +345,7 @@
       const code = String(unit.unitCode || "");
       const rangerStar = rangerStarLayer(rangerStarByCode[code]);
       return `<button class="pvp-player-unit-button" type="button" data-unit-index="${index}" title="${escapeHtml(rangerName(code))}">
-        <span class="pvp-player-unit-image-wrap"><img class="pvp-player-unit-image" src="${RANGER_IMAGE(code)}" alt="" decoding="async" onerror="this.remove();">${unitTalentCorner(unit)}</span>
-        ${teamStarImage(rangerStar, "pvp-player-unit-star")}
+        <span class="pvp-player-unit-image-wrap"><img class="pvp-player-unit-image" src="${RANGER_IMAGE(code)}" alt="" decoding="async" onerror="this.remove();">${unitTalentCorner(unit)}${teamStarImage(rangerStar, "pvp-player-unit-star")}</span>
         <span class="pvp-player-unit-equipment-row">${teamEquipmentSlot(unit, "WEAPON")}${teamEquipmentSlot(unit, "ARMOR")}${teamEquipmentSlot(unit, "ACC")}</span>
         <span class="pvp-player-unit-name">${escapeHtml(rangerName(code))}</span>
       </button>`;
